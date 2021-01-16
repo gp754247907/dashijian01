5f4c1efc39b5e14161f095cc8b89c14f74682495
gp754247907 <754247907@qq.com>
2021-01-16 11:20:04 +0800

    index分支提交

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,16 @@
-$.ajaxPrefilter(function (option) {
-    option.url = 'http://api-breakingnews-web.itheima.net' + option.url
-})
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/Code/login.html'
+        }
+    }
+});
